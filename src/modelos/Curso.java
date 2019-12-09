79049c030d46d3c0deae9c12ff3a71cb2a7f6ee7
TaffarelXavier <taffarel_deus@hotmail.com>
2019-12-08 23:57:38 -0300

    Alterações Importantes

---
+++ src/modelos/Curso.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+/**
+ *
+ * @author Taffarel Xavier <taffarel_deus@hotmail.com>
+ */
+public class Curso {
+
+    private String nome;
+    private int id;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Curso(String nome, int id) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
